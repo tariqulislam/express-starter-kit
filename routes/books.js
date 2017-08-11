96fae8ee3e7009b27ff1502c1611527d1e947785
tariqul islam rony <tariqul.islam.rony@gmail.com>
2017-08-12 00:28:32 +0600

    Add the swagger ui for api

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,21 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let Book = require('../models/Book.js');
 
+/**
+* @swagger
+* definition:
+*   Book:
+*     properties:
+*       title:
+*          type: string
+*       author:
+*          type: string
+*       year:
+*          type: integer
+*       pages:
+*          type: integer
+*/
+
 
 router.get('/', (req,res,next)=> {
   Book.find((err,books) => {
@@ -11,6 +26,28 @@ router.get('/', (req,res,next)=> {
   });
 });
 
+/**
+* @swagger
+* /books/{id}:
+*   get:
+*     tags:
+*       - Books
+*     description: Return a single book
+*     produces:
+*       - application/json
+*     parameters:
+*       - name: id
+*         description: BookId
+*         in: path
+*         required: true
+*         type: string
+*     responses:
+*       200:
+*         description: A single book
+*         schema:
+*           $ref: '#/definitions/Book'
+*
+*/
 router.get('/:id', (req,res,next) => {
   Book.findById(req.params.id, (err,book) => {
     if(err) return next(err);
@@ -18,7 +55,26 @@ router.get('/:id', (req,res,next) => {
   });
 });
 
-
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     tags:
+ *       - Books
+ *     description: Creates a new puppy
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: book
+ *         description: Book object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Book'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.post('/', (req,res, next) => {
   //  Book.create(req.body, (err,book) => {
   //     if(err) return next(err);
