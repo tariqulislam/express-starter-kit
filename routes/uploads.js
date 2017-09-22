0a6bcdf65e0e34741226754960d5bfe4ad800884
Tariqul Islam <tariqul@itconquest.com>
2017-09-22 20:21:14 +0600

    Add file size validation in file upload

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,19 +3,28 @@ var Busboy = require('busboy');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
-var meter = require('stream-meter');
+
 
 
 router.post('/multi', (req, res, next) =>{
     var busboy = new Busboy({ headers: req.headers });
     var messages =[]
     busboy.on("file", function (fieldname, file, filename, encoding, mimetype) {
+        var maxlength = 1 * 1024 * 1024
+        var extentions = ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf'];
+        var size = 0
+        file.on('data', (chunk) => {
+            size += chunk.length;
+        });
 
-        var m = meter();
-        file.pipe(m);
-        console.log("this is meter",m);
-        
-        if(mimetype != 'image/png') {
+        file.on('end', () => {
+          if(size > maxlength) {
+            file.resume();
+            messages.push({"filename": filename, "output": "error", "msg":"file size limit not supported"});
+          }
+        });
+
+        if(extentions.indexOf(mimetype) === -1) {
             file.resume();
             messages.push({"filename": filename, "output": "error", "msg":"file extention not supported"});
         } else {
@@ -23,16 +32,15 @@ router.post('/multi', (req, res, next) =>{
             file.pipe(fs.createWriteStream(saveTo));
             messages.push({"filename": filename, "output": "success", "msg":"file upload successfull"});
         }
-      
+
     });
-        
+
     busboy.on("finish", function () {
         res.status(200).json(messages);
     });
-    
+
     req.pipe(busboy);
-        
-    
+
 });
 
 module.exports = router;
