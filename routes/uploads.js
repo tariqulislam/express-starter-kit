987ec05720c053363ad23ffd1dc0abcf3b27f065
Tariqul islam <tariqul.islam.rony@gmail.com>
2017-09-24 15:59:02 +0600

    change file mimetype restriction

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,39 +3,53 @@ var Busboy = require('busboy');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
-
+var fileType = require('file-type');
 
 
 router.post('/multi', (req, res, next) =>{
     var busboy = new Busboy({ headers: req.headers });
+    console.log("this is headers",req.headers);
     var messages =[]
     busboy.on("file", function (fieldname, file, filename, encoding, mimetype) {
         var maxlength = 1 * 1024 * 1024
         var extentions = ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf'];
         var size = 0
+        var baseBuffer = []
+
+        console.log("this is file",file);
+     
         file.on('data', (chunk) => {
             size += chunk.length;
+            baseBuffer.push(chunk);
         });
 
         file.on('end', () => {
+        
+            let checkfiletype =  fileType(Buffer.concat(baseBuffer,size));
+            if(checkfiletype == null) {
+                checkfiletype = {};
+                checkfiletype['ext'] ='file/invalid';
+                checkfiletype['mime'] = 'file/invalid';
+            }
+            console.log("this is base buffer", checkfiletype.mime)
           if(size > maxlength) {
             file.resume();
             messages.push({"filename": filename, "output": "error", "msg":"file size limit not supported"});
+          } else if(extentions.indexOf(checkfiletype.mime) === -1) {
+              file.resume();
+              messages.push({"filename": filename, "output": "error", "msg":"file is not supported"});
+          } else {
+              const saveTo = path.join(__base, "/uploads/" + filename);
+              file.pipe(fs.createWriteStream(saveTo));
+              messages.push({"filename": filename, "output": "success", "msg":"file upload successfull"});
           }
         });
 
-        if(extentions.indexOf(mimetype) === -1) {
-            file.resume();
-            messages.push({"filename": filename, "output": "error", "msg":"file extention not supported"});
-        } else {
-            const saveTo = path.join(__base, "/uploads/" + filename);
-            file.pipe(fs.createWriteStream(saveTo));
-            messages.push({"filename": filename, "output": "success", "msg":"file upload successfull"});
-        }
 
     });
 
     busboy.on("finish", function () {
+       
         res.status(200).json(messages);
     });
 
